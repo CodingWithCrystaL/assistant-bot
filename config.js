0112bc81c5f46af44267d1ab55a71c670ed2e99d
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-09-14 00:50:42 +0530

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  supportRole: "1375877644314214451", // Role ID allowed to use commands
+  team: {
+    "1399411140655583384": {
+      upi: "payishant@fam",
+      ltc: "ltc1qr354v0qe2mja3rq423g4x80m3lxwcf0vfp5mcv",
+      usdt: "0x1DC2Fdd9Cc33ce107505aA81f1cb4811949ECAAF"
+    },
+    "TEAMMATE_DISCORD_ID": {
+      upi: "teammate-upi@upi",
+      ltc: "teammate-ltc",
+      usdt: "teammate-usdt"
+    }
+  }
+};
