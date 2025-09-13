ff7c6d9f07b9eec1b163b9fa7c660d4046ad2b40
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-09-14 01:50:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,4 @@
 module.exports = {
-  supportRole: "1375877644314214451", // Role ID allowed to use commands
-  team: {
-    "1399411140655583384": {
-      upi: "payishant@fam",
-      ltc: "ltc1qr354v0qe2mja3rq423g4x80m3lxwcf0vfp5mcv",
-      usdt: "0x1DC2Fdd9Cc33ce107505aA81f1cb4811949ECAAF"
-    },
-    "1409093708250611722": {
-      upi: "payishant@fam",
-      ltc: "ltc1qr354v0qe2mja3rq423g4x80m3lxwcf0vfp5mcv",
-      usdt: "0x1DC2Fdd9Cc33ce107505aA81f1cb4811949ECAAF"
-    },
-    "ENTER_TEAMMATE_USER_ID_HERE": { // Replace with actual Discord ID
-      upi: "teammate-upi@upi",
-      ltc: "teammate-ltc",
-      usdt: "teammate-usdt"
-    }
-  }
+  supportRole: "1375877644314214451", // Your support role ID
+  ownerId: "1409093708250611722", // Owner ID
 };
