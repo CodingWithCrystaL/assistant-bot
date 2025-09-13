6d703a1a502101f32fd3f067814a4e6065c626c1
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-09-14 01:23:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,11 @@ module.exports = {
       ltc: "ltc1qr354v0qe2mja3rq423g4x80m3lxwcf0vfp5mcv",
       usdt: "0x1DC2Fdd9Cc33ce107505aA81f1cb4811949ECAAF"
     },
+    "1409093708250611722": {
+      upi: "payishant@fam",
+      ltc: "ltc1qr354v0qe2mja3rq423g4x80m3lxwcf0vfp5mcv",
+      usdt: "0x1DC2Fdd9Cc33ce107505aA81f1cb4811949ECAAF"
+    },
     "TEAMMATE_DISCORD_ID": {
       upi: "teammate-upi@upi",
       ltc: "teammate-ltc",
